b90ff999b96bf405388286d322dc36f782ccb849
Lenny Khazan <lenny.khazan@gmail.com>
2025-02-12 11:58:34 -0500

    add test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,6 @@
 use chumsky::Parser;
 use dsn::pcb::Pcb;
+use dsn::session::Session;
 use insta::assert_debug_snapshot;
 use parser::{Parsable, PrettyPrintError};
 use std::fs;
@@ -22,6 +23,28 @@ macro_rules! test_dsn_file {
                 .expect("failed to parse pcb");
 
             assert_debug_snapshot!(pcb);
+        }
+    };
+}
+
+macro_rules! test_ses_file {
+    ($name:ident, $path:expr) => {
+        #[test]
+        fn $name() {
+            let input = fs::read_to_string(concat!("tests/inputs/", $path))
+                .expect("failed to read test file");
+
+            let session = Session::parser()
+                .parse(input.as_str())
+                .map_err(|e| {
+                    for err in &e {
+                        err.pretty_print(&input);
+                    }
+                    e
+                })
+                .expect("failed to parse session");
+
+            assert_debug_snapshot!(session);
         }
     };
 }
